6574a698e2535fd391e0496dc7b7c7b26fc75e9b
Pooja <poojadahiya121001@gmail.com>
2022-06-29 20:15:11 +0530

    add

---
+++ pattern/pattern.js
@@ -0,0 +1,113 @@
+// Question 1
+// Print the pattern given below, using loops.
+
+// Input:
+// No input is required.
+
+// Output:
+
+// *****
+// *****
+// *****
+// ***** 
+
+
+// Answer
+// let num2=5;
+// let num1=4;
+
+//  for(let i=1;i<=num1;i++){
+//    let stars=''
+//    for(let j=1;j<=num2;j++){
+//      stars+='*'
+//    }
+//    console.log(stars)
+//  }
+
+
+// Question 2
+// Print the pattern given below, using loops.
+
+// Input:
+// No input is required.
+
+// Output:
+
+// *
+// **
+// ***
+// ****
+// ***** 
+
+
+// Answer
+
+// let num=5;
+ 
+//  for(let i=1;i<=num;i++){
+//     let stars=''
+//     for(let j=1;j<=i;j++){
+//       stars=stars+'*'
+//     }
+//     console.log(stars)
+//  }
+
+
+// Question 3
+// Print the pattern given below, using loops.
+
+// Input:
+// No input is required.
+
+// Output:
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+// Answer
+
+// let num =5
+// let count=1
+// for(i=1;i<=5;i++){
+//   let stars=' '
+//   for(j=1;j<=i;j++){
+//     stars=stars+count
+//     count++
+//   }
+//   console.log(stars)
+// }
+
+
+// Question- 4
+// Print the pattern given below, using loops.
+
+// Input:
+// No input is required.
+
+// Output:
+// 1
+// 23
+// 123
+// 1231
+// 23123
+
+
+//  Answer-4 
+// let num=5;
+//  let count=1
+ 
+//  for(let i=1;i<=num;i++){
+//     let stars=''
+//     for(let j=1;j<=i;j++){
+//       if(count>3){
+//         count=1;
+//       }
+//       stars+=
+//       count++
+     
+//     }
+//     console.log(stars)
+//  }
+
